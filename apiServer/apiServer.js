4bd4629d62ee0ab56698fe3c6a9ed2afedba0002
Daniel Ye <dye@ca.ibm.com>
2016-12-18 21:33:58 -0500

    Implement pendingFeedbacks API call

--- apiServer/apiServer.js
+++ apiServer/apiServer.js
@@ -14,6 +14,31 @@ app.get('/api/v1/users/', function (req, res) {
    });
 })
 
+app.get('/api/v1/users/:id/pendingFeedbacks', function (req, res) {
+   fs.readFile( __dirname + "/" + "reviews.json", 'utf8', function (err, data) {
+   		var reviews = JSON.parse(data).reviews;
+   		var pendingFeedbacks = [];
+   		for (var i = 0; i < reviews.length; i++) {
+   			if (reviews[i].assignees.indexOf(Number(req.params.id)) > -1) {
+   				var target = reviews[i].target;
+   				if (pendingFeedbacks.indexOf(target) < 0) {
+   					pendingFeedbacks.push(target);
+   				}
+   			}
+   		}
+		res.writeHeader(200, {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods':'GET, OPTIONS',
+			'Access-Control-Allow-Headers': 'Origin, Content-Type'
+		})
+		var result = {
+			pendingFeedbacks
+		};
+
+    	res.end( JSON.stringify(result) );
+   });
+})
+
 app.get('/api/v1/reviews', function (req, res) {
    fs.readFile( __dirname + "/" + "reviews.json", 'utf8', function (err, data) {
    		console.log( data );
